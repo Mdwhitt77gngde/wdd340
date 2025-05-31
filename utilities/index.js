d12e32b20a1789de8e9459c03381144ac0c606f8
mdwhitt77gngde <u585507859@byui.edu>
2025-05-31 14:05:18 -0500

    up5

--- utilities/index.js
+++ utilities/index.js
@@ -8,7 +8,7 @@ Util.getNav = async function (req, res, next) {
     let data = await invModel.getClassifications()
     console.log(data)// added 
     let list = "<ul>"
-    list += '<li><a href="/"title="Home page">Home</a></li>'
+    list += '<li><a href="/" title="Home page">Home</a></li>'
     data.rows.forEach((row) => {
         list += "<li>"
         list +=
