17ae15c90932d7040d6c85cf1013748cfa1de853
mdwhitt77gngde <u585507859@byui.edu>
2025-06-10 09:32:48 -0500

    invroutefix

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,25 +1,24 @@
 // routes/inventoryRoute.js
-const express         = require("express")
-const router          = express.Router()
-const utilities       = require("../utilities")
-const invController   = require("../controllers/invController") // ← FIXED: match file name
+const express           = require('express')
+const router            = express.Router()
+const utilities         = require('../utilities')
+const invController     = require('../controllers/invController')  // matches controllers/invController.js
+const classValidate     = require('../utilities/classification-validation')
+const invValidate       = require('../utilities/inventory-validation')
 
-const classValidate   = require("../utilities/classification-validation")
-const invValidate     = require("../utilities/inventory-validation")
-
-// Task 1: Management View
+// Task 1: Management View (/inv/)
 router.get(
-  "/",
+  '/',
   utilities.handleErrors(invController.buildManagement)
 )
 
 // Task 2: Add Classification
 router.get(
-  "/add-classification",
+  '/add-classification',
   utilities.handleErrors(invController.buildAddClassification)
 )
 router.post(
-  "/add-classification",
+  '/add-classification',
   classValidate.classificationRules(),
   classValidate.checkClassificationData,
   utilities.handleErrors(invController.registerClassification)
@@ -27,25 +26,25 @@ router.post(
 
 // Task 3: Add Inventory
 router.get(
-  "/add-inventory",
+  '/add-inventory',
   utilities.handleErrors(invController.buildAddInventory)
 )
 router.post(
-  "/add-inventory",
+  '/add-inventory',
   invValidate.inventoryRules(),
   invValidate.checkInventoryData,
   utilities.handleErrors(invController.registerInventory)
 )
 
-// Classification View
+// Classification View (/inv/type/:classification_Id)
 router.get(
-  "/type/:classification_Id",
+  '/type/:classification_Id',
   utilities.handleErrors(invController.buildByClassificationId)
 )
 
-// Vehicle Detail
+// Vehicle Detail (/inv/detail/:inv_id)
 router.get(
-  "/detail/:inv_id",
+  '/detail/:inv_id',
   utilities.handleErrors(invController.buildDetail)
 )
 
